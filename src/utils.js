399a35964b45f5a879df16be30da711533b3ceed
akibhasan50 <tonuakibhasan@gmail.com>
2022-03-12 00:28:50 +0600

    custom pagination with react

---
+++ src/utils.js
@@ -0,0 +1,13 @@
+const paginate = (followers) => {
+  const itemPerPage = 9;
+  const pages = Math.ceil(followers.length / itemPerPage);
+
+  const newFollowers = Array.from({ length: pages }, (_, index) => {
+    const start = index * itemPerPage;
+    return followers.slice(start, start + itemPerPage);
+  });
+
+  return newFollowers;
+};
+
+export default paginate;
